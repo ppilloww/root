0484a6c6de6b390760dc3fa78aa9850a7a2f90ea
ppilloww <pandora.temp@outlook.com>
2024-05-08 20:54:38 +0200

    reset database to factory

--- bbq_gmbh_app/admin.py
+++ bbq_gmbh_app/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from .models import Adresse, Mitarbeiter, Abteilung, Arbeitsstunden, Urlaub, Abteilungsleiter #migration Database
+
 # Register your models here.
 
 
 ######################################### Database operations #########################################
  
-admin.site.register(Adresse)
-admin.site.register(Mitarbeiter)
-admin.site.register(Abteilung)
-admin.site.register(Abteilungsleiter)
-admin.site.register(Arbeitsstunden)
-admin.site.register(Urlaub)
+
 
 ######################################### Database operations #########################################
--- bbq_gmbh_app/views.py
+++ bbq_gmbh_app/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
 from datetime import date, datetime
 import holidays
-from bbq_gmbh_app.models import Adresse, Mitarbeiter, Abteilungsleiter, Abteilung
-from django.contrib.auth import authenticate, login, logout
+
 
 
 # Create your views here.
@@ -123,128 +122,7 @@ def user_view(request):
 
 ######################################### Database operations #########################################
 
-def create_mitarbeiter(strasse, hausnummer, stadt, plz, land, vorname, nachname, email, geburtsdatum, position, gehalt, hr_tag, passwort, admin_tag, urlaubstage, wochenstundensatz, ueberstunden, geschlecht, quartalueberstunden, jahresueberstunden, abteilungsleiter_vorname, abteilungsleiter_nachname, abteilungsleiter_email,abteilungsname):
-    # Überprüfen, ob die Adresse existiert
-    try:
-        adresse = Adresse.objects.get(strasse=strasse, hausnummer=hausnummer, stadt=stadt, plz=plz, land=land)
-    except Adresse.DoesNotExist:
-        # Wenn die Adresse nicht existiert, erstellen Sie eine neue
-        adresse = Adresse.objects.create(strasse=strasse, hausnummer=hausnummer, stadt=stadt, plz=plz, land=land)
-        
-    try:
-        abteilungsleiter = Abteilungsleiter.objects.get(vorname=abteilungsleiter_vorname, nachname=abteilungsleiter_nachname, email=abteilungsleiter_email)
-    except Adresse.DoesNotExist:
-        # Wenn die Adresse nicht existiert, erstellen Sie eine neue
-        abteilungsleiter = Abteilungsleiter.objects.create(vorname=abteilungsleiter_vorname, nachname=abteilungsleiter_nachname, email=abteilungsleiter_email)
-         
-           
-    try:
-        abteilung = Abteilung.objects.get(abteilungsname=abteilungsname)
-    except Adresse.DoesNotExist:
-        abteilung = Abteilung.objects.create(abteilungsname=abteilungsname, abteilungsleiter=abteilungsleiter)
-      
-         
 
-    # Überprüfen, ob der Mitarbeiter existiert
-    try:
-        mitarbeiter = Mitarbeiter.objects.get(vorname=vorname, nachname=nachname, email=email)
-        
-    except Mitarbeiter.DoesNotExist:
-        # Wenn der Mitarbeiter nicht existiert, erstellen Sie einen neuen
-        mitarbeiter = Mitarbeiter.objects.create(
-            adresse=adresse,
-            vorname=vorname,
-            nachname=nachname,
-            email=email,
-            geburtsdatum=datetime.strptime(geburtsdatum, '%Y-%m-%d').date(),
-            position=position,
-            gehalt=gehalt,
-            hr_tag=hr_tag,
-            passwort=passwort,
-            admin_tag=admin_tag,
-            urlaubstage=urlaubstage,
-            wochenstundensatz=wochenstundensatz,
-            ueberstunden=ueberstunden,
-            geschlecht=geschlecht,
-            quartalueberstunden=quartalueberstunden,
-            jahresueberstunden=jahresueberstunden
-    )
-        
-def mitarbeiter_loeschen(mitarbeiter_id=None, vorname=None, nachname=None):
-    if mitarbeiter_id:
-        filter_args = {'id': mitarbeiter_id}
-    elif vorname and nachname:
-        filter_args = {'vorname': vorname, 'nachname': nachname}
-    else:
-        return False, "Bitte geben Sie eine Mitarbeiter-ID oder Vor- und Nachnamen an."
-
-    try:
-        mitarbeiter = Mitarbeiter.objects.get(**filter_args)
-        abteilungen_leitung = Abteilung.objects.filter(abteilungsleiter=mitarbeiter)
-
-        # Setze den Abteilungsleiter der betroffenen Abteilungen auf NULL
-        for abteilung in abteilungen_leitung:
-            abteilung.abteilungsleiter = None
-            abteilung.save()
-
-        # Lösche die Adresse des Mitarbeiters
-        mitarbeiter.adresse.delete()
-        # Lösche alle Arbeitsstunden des Mitarbeiters
-        mitarbeiter.arbeitsstunden.all().delete()
-        # Lösche alle Urlaube des Mitarbeiters
-        mitarbeiter.urlaube.all().delete()
-        # Lösche den Mitarbeiter selbst
-        mitarbeiter.delete()
-
-        return True, f"Mitarbeiter {mitarbeiter} und alle zugehörigen Daten wurden erfolgreich gelöscht."
-    except Mitarbeiter.DoesNotExist:
-        return False, f"Mitarbeiter mit den angegebenen Daten wurde nicht gefunden."
-    except Exception as e:
-        return False, f"Fehler beim Löschen des Mitarbeiters: {str(e)}"
-
-
-
-
-def mitarbeiter_aktualisieren(mitarbeiter_id):
-    try:
-        mitarbeiter = Mitarbeiter.objects.get(id=mitarbeiter_id)
-        adresse = mitarbeiter.adresse
-        
-        neue_daten = erstelle_neue_daten_mitarbeiter_und_adresse(mitarbeiter_id)
-        
-        if neue_daten:
-            mitarbeiter.update(**neue_daten)
-            adresse.update(**neue_daten)
-
-            return True, f"Mitarbeiter {mitarbeiter_id} und Adresse wurden erfolgreich aktualisiert."
-        else:
-            return False, f"Mitarbeiter mit der ID {mitarbeiter_id} wurde nicht gefunden."
-    except Exception as e:
-        return False, f"Fehler beim Aktualisieren des Mitarbeiters und der Adresse: {str(e)}"
-
-
-
-
-def erstelle_neue_daten_mitarbeiter_und_adresse(mitarbeiter_id, **kwargs):
-    try:
-        mitarbeiter = Mitarbeiter.objects.get(id=mitarbeiter_id)
-        adresse = mitarbeiter.adresse
-
-        # Extrahiere die Feldnamen der Mitarbeiter-Tabelle
-        mitarbeiter_feldnamen = [feld.name for feld in Mitarbeiter._meta.get_fields() if feld.name != 'id']
-
-        # Extrahiere die Feldnamen der Adresse-Tabelle
-        adresse_feldnamen = [feld.name for feld in Adresse._meta.get_fields() if feld.name != 'id']
-
-        # Kombiniere die Feldnamen beider Tabellen
-        alle_feldnamen = mitarbeiter_feldnamen + adresse_feldnamen
-
-        # Erstelle das Wörterbuch mit allen Feldern und ihren aktuellen Werten
-        neue_daten = {feld: getattr(mitarbeiter, feld) for feld in alle_feldnamen}
-
-        return neue_daten
-    except Mitarbeiter.DoesNotExist:
-        return None
 
 
 ######################################### Database operations #########################################
