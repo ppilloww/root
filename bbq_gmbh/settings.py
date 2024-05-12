1cf19fa2d2f35748004cfce98e380a1c55fca741
ppilloww <pandora.temp@outlook.com>
2024-05-12 16:01:58 +0200

    reset Database

--- bbq_gmbh/settings.py
+++ bbq_gmbh/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h0erxc9)5%h)3eo(qwcd@!s4zgcusnn4lr7_m6ts^p0g-(d9l*
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost','127.0.0.1'
+    'localhost','127.0.0.1',
 ]
 
 
@@ -120,12 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
 
-# ]
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
