11b06a33debc9b8cc20f91a6d90c3f67d2a7eae8
ppilloww <pandora.temp@outlook.com>
2024-05-24 19:23:40 +0200

    Added session, all templates, created views, created urls, created a user. life is beautiful again

--- bbq_gmbh_app/static/bbq_gmbh_app/js/scripts.js
+++ bbq_gmbh_app/static/bbq_gmbh_app/js/scripts.js
@@ -116,3 +116,67 @@ window.onload = function() {
     var element = document.getElementById('myElement');
     element.scrollIntoView({behavior: "smooth", block: "center"});
 }
+
+
+// Navicon
+
+function executeMyCode(user) {
+    var navbar = document.querySelector('.navbar-nav.ms-auto');
+
+    if (user.tag === "admin" || user.tag === "hr") {
+        // Create new nav item and link
+        var employeeManagementUrl = document.body.dataset.employeeManagementUrl;
+        var newItem = document.createElement('li');
+        newItem.className = 'nav-item';
+
+        var newLink = document.createElement('a');
+        newLink.className = 'nav-link';
+        newLink.href = employeeManagementUrl;
+        newLink.textContent = 'Employee management';
+
+        // Append new link to new item
+        newItem.appendChild(newLink);
+
+        // Find the position where the new item should be inserted
+        var refItem = document.querySelector('.navbar-nav.ms-auto .nav-item:nth-last-child(2)');
+
+        // Insert the new item before the reference item
+        navbar.insertBefore(newItem, refItem);
+    }
+}
+
+// fetch user tag
+fetch('/get_user_role/')
+.then(response => response.json())
+.then(data => {
+    if (data.user_role) {
+
+        // Icon byUsername
+        var user = {
+            tag: data.user_role // this could be 'admin', 'hr', 'ma'
+        };
+
+        // droptown by ID
+        var dropdownItem = document.getElementById('user');
+
+        // Create an icon element
+        var icon = document.createElement('i');
+        icon.style.marginRight = '5px'; // Add some space between the icon and the text
+
+        // logic
+        if (user.tag === 'admin') {
+            icon.className = 'fa-solid fa-user-plus text-danger';
+        } else if (user.tag === 'hr') {
+            icon.className = 'fa-solid fa-user-tie text-warning';
+        } else {
+            icon.className = 'fa-solid fa-user text-primary';
+        }
+
+        // Add icon to dropdown item
+        dropdownItem.prepend(icon);
+
+        // Execute the function with the user object
+        executeMyCode(user);
+    }
+})
+.catch(error => console.error('Error:', error));
--- bbq_gmbh_app/urls.py
+++ bbq_gmbh_app/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin, name='signin'),
+    path('home/', views.home, name='home'),
+    path('employeeManagement/', views.employeeManagement, name='employeeManagement'),
+    path('profile/', views.profile, name='profile'),
+    path('changePassword/', views.changePassword, name='changePassword'),
+    path('logout/', views.userLogout, name='logout'),
+    path('createUser/', views.createUser, name='createUser'),
     
 ]
