55cd34293a76349af9edea5197886388e2ce4585
DavidMi98 <118054417+DavidMi98@users.noreply.github.com>
2024-07-11 23:17:36 +0200

    last rewind

--- website/settings.py
+++ website/settings.py
@@ -130,8 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
