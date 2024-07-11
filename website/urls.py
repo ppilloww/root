233dbf41b5846b93e3ce08f394d17f4206562d8d
Pawel <zurawel.pawet@gmx.de>
2024-07-11 21:06:05 +0200

    Translation_almost_done

--- website/urls.py
+++ website/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('', include('bbq_gmbh_app.urls')),
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-]
+    path('rosetta/', include('rosetta.urls')),
+    path('', include('bbq_gmbh_app.urls')),
+)
