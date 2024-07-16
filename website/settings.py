a046a84b84946aa69c1038bc178d1b84b0ff785f
DavidMi98 <118054417+DavidMi98@users.noreply.github.com>
2024-07-16 22:45:40 +0200

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -125,15 +125,15 @@ AUTH_USER_MODEL = 'bbq_gmbh_app.Mitarbeiter'
 
 ############## PRODUCTION SETTINGS ####################
 
-#CSRF_COOKIE_SECURE = True
-#SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
-#CSRF_TRUSTED_ORIGINS = [
-#    'https://bbq-fom.azurewebsites.net',
-#    'http://bbq-fom.azurewebsites.net', # HTTPS and HTTP versions of a domain is not typically recommended for production environments due to the potential security risks associated with non-secure HTTP traffic
-#]
+CSRF_TRUSTED_ORIGINS = [
+    'https://bbq-gmbh2.azurewebsites.net',
+    'http://bbq-gmbh2.azurewebsites.net', # HTTPS and HTTP versions of a domain is not typically recommended for production environments due to the potential security risks associated with non-secure HTTP traffic
+]
 
-#SECURE_SSL_REDIRECT = True #a security feature that, when set to True, ensures that all HTTP requests to your application are redirected to HTTPS
+SECURE_SSL_REDIRECT = True #a security feature that, when set to True, ensures that all HTTP requests to your application are redirected to HTTPS
 
 
 LANGUAGE_CODE = 'en'
